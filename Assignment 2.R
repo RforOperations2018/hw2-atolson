2c8a524aa7e7244c1aadd44d344f864a7f248396
arcanaknight1 <33413009+arcanaknight1@users.noreply.github.com>
2018-09-14 19:12:50 -0400

    Created inputs

--- Assignment 2.R
+++ Assignment 2.R
@@ -16,42 +16,66 @@ employ <- read.csv(file="employment.csv", header = TRUE, sep = ",")
 pdf(NULL)
 
 # Define UI for application
-ui <- navbarPage("Pittsburgh Employment Statistics", 
-                 tabPanel("Plot",
-                          sidebarLayout(
-                            sidebarPanel(
-                              # Neighborhood select
-                              selectInput("HoodSelect",
-                                          "Neighborhood:",
-                                          choices = sort(employ$Neighborhood),
-                                          multiple = TRUE,
-                                          selectize = TRUE,
-                                          selected = c("North Oakland", "Squirrel Hill North", "Squirrel Hill South")),
-                              # Population Selection
-                              sliderInput("popSelect",
-                                          "Population:",
-                                          min = min(employ$Population.2010, na.rm = T),
-                                          max = max(employ$Population.2010, na.rm = T),
-                                          value = c(min(employ$Population.2010, na.rm = T), max(employ$Population.2010, na.rm = T)),
-                                          step = 1),
-                              actionButton("reset", "Reset Filters", icon = icon("refresh"))
-                            ),
-                            # Output plot
-                            mainPanel(
-                              plotlyOutput("plot")
-                            )
-                          )
-                 ),
-                 # Data Table
-                 tabPanel("Table",
-                          inputPanel(
-                            downloadButton("downloadData","Download Employment Data")
-                          ),
-                          fluidPage(DT::dataTableOutput("table"))
-                 )
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("Pittsburgh Employment Statistics"),
+  
+  # Sidebar
+  sidebarLayout(
+    sidebarPanel(
+      
+      #Neighborhood select
+      selectInput("HoodSelect",
+                  "Neighborhood:",
+                  choices = sort(employ$Neighborhood),
+                  multiple = TRUE,
+                  selectize = TRUE,
+                  selected = c("North Oakland", "Squirrel Hill North", "Squirrel Hill South")),
+      
+      #Population Selection
+      sliderInput("popSelect",
+                  "Population:",
+                  min = min(employ$Population.2010, na.rm = T),
+                  max = max(employ$Population.2010, na.rm = T),
+                  value = c(min(employ$Population.2010, na.rm = T), max(employ$Population.2010, na.rm = T)),
+                  step = 1),
+      
+      #Reset Filters button
+      actionButton("reset", "Reset Filters", icon = icon("refresh")),
+      
+      #Download data button
+      downloadButton("downloadData","Download Employment Data")
+    ),
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Plot1", 
+                 plotlyOutput("plot1")
+        ),
+        tabPanel("Plot2", 
+                 plotlyOutput("plot2")
+        ),
+        tabPanel("Plot3", 
+                 plotlyOutput("plot3")
+        ),
+        tabPanel("Table",
+                 DT::dataTableOutput("table")
+        )
+      )
+    )
+  )
 )
 
 # Define server logic
+server <- function(input, output) {
+  output$plot1 <- renderPlotly({
+    dat <- subset(employ, Neighborhood %in% input$HoodSelect)
+    ggplot(data = dat, aes(x = Neighborhood, y = as.numeric(Total_Adult_Residents_Employed.2010), fill = Neighborhood)) + geom_bar(stat = "identity")
+  })
+  output$table <- DT::renderDataTable({
+    subset(employ, Neighborhood %in% input$HoodSelect, select = c(Employment, Population.2010, Sector, Total_Adult_Residents_Employed.2010))
+  })
+}
 
 # Run the application 
 shinyApp(ui = ui, server = server, enableBookmarking = "url")
